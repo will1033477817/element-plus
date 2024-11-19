4a3fa926f609f63141c9b2017bcc0f308a66a707
will1033477817 <62694483+will1033477817@users.noreply.github.com>
2024-11-19 12:22:48 +0800

    Update expandable-row.vue

--- docs/examples/table/expandable-row.vue
+++ docs/examples/table/expandable-row.vue
@@ -2,7 +2,7 @@
   switch parent border: <el-switch v-model="parentBorder" /> switch child
   border: <el-switch v-model="childBorder" />
   <el-table :data="tableData" :border="parentBorder" style="width: 100%">
-    <el-table-column type="expand">
+    <el-table-column type="slot">
       <template #default="props">
         <div m="4">
           <p m="t-0 b-2">State: {{ props.row.state }}</p>
